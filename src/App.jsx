b8391d023d87b0f4bd69c3b64d9c05e34592728b
heet214 <heetzatakia123@gmail.com>
2025-08-12 19:19:38 +0530

    FEAT: Added Announcements Page and Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,39 @@
-import React, { useState } from 'react';
-import { Routes, Route, Outlet } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Header from './components/Header';
-import StakeholderPage from './pages/StakeholderPage';
-import LoginPage from './pages/LoginPage';
+import React, { useState } from "react";
+import { Routes, Route, Outlet } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import StakeholderPage from "./pages/StakeholderPage";
+import LoginPage from "./pages/LoginPage";
+import AnnouncementsPage from "./pages/AnnouncementsPage";
 
 const AppLayout = () => {
-  const [isDesktopExpanded, setDesktopExpanded] = useState(true);
-  const [isMobileOpen, setMobileOpen] = useState(false);
+	const [isDesktopExpanded, setDesktopExpanded] = useState(true);
+	const [isMobileOpen, setMobileOpen] = useState(false);
 
-  return (
-    // Set the main background to the light gray from the design
-    <div className="flex h-screen bg-gray-50 font-sans">
-      <Sidebar
-        isDesktopExpanded={isDesktopExpanded}
-        setDesktopExpanded={setDesktopExpanded}
-        isMobileOpen={isMobileOpen}
-        setMobileOpen={setMobileOpen}
-      />
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <Header setMobileOpen={setMobileOpen} />
-        <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto">
-          <Outlet />
-        </main>
-      </div>
-    </div>
-  );
+	return (
+		// Set the main background to the light gray from the design
+		<div className="flex h-screen bg-gray-50 font-sans">
+			<Sidebar isDesktopExpanded={isDesktopExpanded} setDesktopExpanded={setDesktopExpanded} isMobileOpen={isMobileOpen} setMobileOpen={setMobileOpen} />
+			<div className="flex-1 flex flex-col overflow-hidden">
+				<Header setMobileOpen={setMobileOpen} />
+				<main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto">
+					<Outlet />
+				</main>
+			</div>
+		</div>
+	);
 };
 
 function App() {
-  return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route path="/" element={<StakeholderPage />} />
-        {/* Add other routes here */}
-      </Route>
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route element={<AppLayout />}>
+				<Route path="/" element={<StakeholderPage />} />
+				<Route path="/announcements" element={<AnnouncementsPage />} />
+			</Route>
+			<Route path="/login" element={<LoginPage />} />
+		</Routes>
+	);
 }
 
 export default App;
